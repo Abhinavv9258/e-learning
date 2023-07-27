8057eb75f8cd39314bd43eae1f9f349ef692628e
Abhinav Verma <abhinavv218@gmail.com>
2023-07-27 20:25:06 +0530

    server

---
+++ server/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.l5gayqw.mongodb.net/E-Learn?retryWrites=true&w=majority`;
+
+const connexion = mongoose.connect(uri, connectionParams)
+.then(() => console.log('connection established'))
+.catch((error) => console.log(error));
+
+module.exports =  connexion
--- server/index.js
+++ server/index.js
@@ -79,28 +79,42 @@
 //         console.error(`Error connecting to the database. \n${err}`);
 //     })
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://abhinavv2180:abhi6BBYM@cluster0.l5gayqw.mongodb.net/?retryWrites=true&w=majority";
+const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+const db = require('./connection');
+const app = express();
+// app.use(cors());
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+app.listen(3030, () => {
+  console.log('Listning to port 3030...');
+})
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://abhinavv2180:abhi6BBYM@cluster0.l5gayqw.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
