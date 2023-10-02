f9b27c2c319e5d2686cf877bee9bfc031386ab02
Abhinav Verma <abhinavv218@gmail.com>
2023-10-03 01:03:14 +0530

    server

--- src/components/UserLoginForm.js
+++ src/components/UserLoginForm.js
@@ -39,44 +39,54 @@ const UserLoginForm = () => {
                 autoClose: 3000,
             });
         } else {
-            try{
+            try {
                 const data = await fetch("/api/auth/login", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
                     },
                     body: JSON.stringify({
-                        username, 
+                        username,
                         password
                     })
                 });
 
                 if (data.status === 200) {
                     // Login successful
-                    try{
-                    const res = await data.json();
-                    // Assuming the server response includes the user's name
-                    setUser({
-                        username: res.user.username, // Adjust this according to your server response
-                        token: res.token,
-                    });
-                    toast.success(`User Logged In! Welcome, ${res.user.username}`, {
-                        position: toast.POSITION.TOP_RIGHT,
-                        autoClose: 3000,
-                    });
-                    
-                    localStorage.setItem("access_token", res.token);
+                    try {
+                        const res = await data.json();
+                        if (res && res.user && res.token) {
 
-                    // Redirect to a different page, e.g., the user's dashboard
-                    navigate("/");
-                } catch (jsonError) {
-                    // Handle JSON parsing error
-                    console.error("Error parsing JSON response:", jsonError);
-                    toast.error("An error occurred during login. Please try again later.", {
-                        position: toast.POSITION.TOP_RIGHT,
-                        autoClose: 3000,
-                    });
-                }
+                            // Assuming the server response includes the user's name
+                            setUser({
+                                username: res.user.username, // Adjust this according to your server response
+                                token: res.token,
+                            });
+                            toast.success(`User Logged In! Welcome, ${res.user.username}`, {
+                                position: toast.POSITION.TOP_RIGHT,
+                                autoClose: 3000,
+                            });
+
+                            localStorage.setItem("access_token", res.token);
+
+                            // Redirect to a different page, e.g., the user's dashboard
+                            navigate("/");
+                        } else {
+                            // Handle unexpected response structure
+                            console.error("Unexpected response structure:", res);
+                            toast.error("An error occurred during login. Please try again later.", {
+                                position: toast.POSITION.TOP_RIGHT,
+                                autoClose: 3000,
+                            });
+                        }
+                    } catch (jsonError) {
+                        // Handle JSON parsing error
+                        console.error("Error parsing JSON response:", jsonError);
+                        toast.error("An error occurred during login. Please try again later.", {
+                            position: toast.POSITION.TOP_RIGHT,
+                            autoClose: 3000,
+                        });
+                    }
                 } else {
                     // Login failed, handle the error message from the server
                     const response = await data.json();
@@ -109,43 +119,43 @@ const UserLoginForm = () => {
                 <div className="card border-0">
                     <div className="content row no-gutters d-flex justify-content-center align-items-center">
                         <div className="register-image-col col">
-                        <img src={login} style={{width:"50%"}} className="card-img img-fluid" alt="..."/>
+                            <img src={login} style={{ width: "50%" }} className="card-img img-fluid" alt="..." />
                         </div>
                         <div className='form-col col'>
                             <div className="card-body">
                                 <form onSubmit={handleSubmit}>
-                                <FormGroup className='d-flex justify-content-center align-items-center' style={{width:"75%"}}>
-                                    <FormControl className='register-form-title card-title'>
-                                        <h5 class="widget-title" style={{"margin-bottom": "20px"}}> Sign up and start learning 
-                                            <span style={{"background": "#839FAD none repeat scroll 0 0","display":"block","height":"1.5px","margin-top":"10px","position":"relative","width":"20%"}}></span>
-                                            <span style={{"background":"#839FAD none repeat scroll 0 0","display":"block",height:"1.5px","margin-top":"5px","width":"50%"}}></span>
-                                        </h5>
-                                    </FormControl>
-                                    <FormControl className='register-input-label'>
-                                        <InputLabel>User-Name/Email</InputLabel>
-                                        <Input type="text" id="username" 
-                                        onChange={handleChange}
-                                        value={credentials.username} // Bind the input value to state
-                                        />
-                                    </FormControl>
-                                    <FormControl className='register-input-label' >
-                                        <InputLabel>Password</InputLabel>
-                                        <Input type="password" 
-                                        id="password" 
-                                        onChange={handleChange}
-                                        value={credentials.password} // Bind the input value to state
-                                        />
-                                    </FormControl>
-                                    <FormControl style={{display:"flex","justify-content":"space-evenly",alignItems:"center","flex-direction":"row"}}>
+                                    <FormGroup className='d-flex justify-content-center align-items-center' style={{ width: "75%" }}>
+                                        <FormControl className='register-form-title card-title'>
+                                            <h5 class="widget-title" style={{ "margin-bottom": "20px" }}> Sign up and start learning
+                                                <span style={{ "background": "#839FAD none repeat scroll 0 0", "display": "block", "height": "1.5px", "margin-top": "10px", "position": "relative", "width": "20%" }}></span>
+                                                <span style={{ "background": "#839FAD none repeat scroll 0 0", "display": "block", height: "1.5px", "margin-top": "5px", "width": "50%" }}></span>
+                                            </h5>
+                                        </FormControl>
+                                        <FormControl className='register-input-label'>
+                                            <InputLabel>User-Name/Email</InputLabel>
+                                            <Input type="text" id="username"
+                                                onChange={handleChange}
+                                                value={credentials.username} // Bind the input value to state
+                                            />
+                                        </FormControl>
+                                        <FormControl className='register-input-label' >
+                                            <InputLabel>Password</InputLabel>
+                                            <Input type="password"
+                                                id="password"
+                                                onChange={handleChange}
+                                                value={credentials.password} // Bind the input value to state
+                                            />
+                                        </FormControl>
+                                        <FormControl style={{ display: "flex", "justify-content": "space-evenly", alignItems: "center", "flex-direction": "row" }}>
                                             <Button className='homepage-btn' type="submit" > LogIn </Button>
-                                    </FormControl>
-                                    <Button className='homepage-btn' type="submit" value="submit" > SignUp </Button>
-                                </FormGroup>
+                                        </FormControl>
+                                        <Button className='homepage-btn' type="submit" value="submit" > SignUp </Button>
+                                    </FormGroup>
                                 </form>
                             </div>
                         </div>
                     </div>
-                </div> 
+                </div>
             </div>
             <ToastContainer />
         </div>
