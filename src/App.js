a7cdb5afad75d8e32fa7cb23d6812d1358dd6d4b
Abhinav Verma <79451162+Abhinavv9258@users.noreply.github.com>
2023-10-07 19:58:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,34 @@
 import * as React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// importing pages 
 import Index from './pages/Index'
 import RegisterPage from './pages/RegisterPage'
 import UserLoginPage from './pages/UserLoginPage'
-// import AdminLoginPage from './pages/AdminLoginPage'
-// import AdminDashboard from './pages/AdminDashboard'
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+
+// importing styles
 import './App.css';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; // Import the CSS for styling
 
-
-// const App = () => {
-//     const dispatch = useDispatch();
-//     let state = useSelector((state) => state);
-//     // let state = useSelector((state) => state.apiAddDataReducer.response);
-//     React.useEffect(() => {
-//         dispatch(getDataList.getData());
-//     }, [dispatch]);
-//     return (
-//         <BrowserRouter>
-//           {
-//               state?.apiDataReducer?.isSuccess &&
-//               <Routes>
-//                     <Route path='/' element={ <Appjs dataRes={state?.apiDataReducer?.response} /> } />
-//                   <Route path='/login' element={ <LoginPage dataRes={state?.apiDataReducer?.response} /> } />
-//                   <Route path='/register' element={ <RegisterPage dataRes={state?.apiDataReducer?.response} /> } />
-//               </Routes>
-//           }
-//           <RegisterPage />
-//         </BrowserRouter>
-//     );
-// }
+// exporting server URL
 export const URL = process.env.REACT_APP_SERVER_URL;
 
 const App = () => {
-        return (
+
+    return (
         <BrowserRouter>
             <ToastContainer />
-              <Routes>
-                    <Route path='/' element={ <Homepage/> } />
-                    <Route path='/RegisterPage' element={ <RegisterPage/> } />
-                    <Route path='/Homepage' element={ <Homepage/> } />
-                    {/* <Route path='/AdminDashboard' element={ <AdminDashboard/> } /> */}
-                    <Route path='/Index' element={ <Index/> } />
-                    {/* <Route path='/adminLoginPage' element={ <AdminLoginPage/> } /> */}
-                    <Route path='/userLoginPage' element={ <UserLoginPage/> } />
-              </Routes>
+            <Routes>
+                <Route path='/' element={<Index />} />
+                <Route path='/RegisterPage' element={<RegisterPage />} />
+                <Route path='/Homepage' element={<Homepage />} />
+                <Route path='/Index' element={<Index />} />
+                <Route path='/userLoginPage' element={<UserLoginPage />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
         </BrowserRouter>
     );
 }
