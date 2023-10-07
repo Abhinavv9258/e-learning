811610be4baaec058475df7108e6622364a7e3c5
Abhinav Verma <79451162+Abhinavv9258@users.noreply.github.com>
2023-10-07 19:00:10 +0530

    Revert "none"
    This reverts commit 5394560d0a44206ea0467611b239ba31129e3def.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
     const navigate = useNavigate();
     return (
         <>
-        <footer id="footer" class="footer-1">
+        <footer id="footer" class="footer-1" style={{"margin-top":"20px",padding:"15px 0 15px 0","box-shadow": "0px -3px 1px -2px rgba(0,0,0,0.2), 0px -2px 2px 0px rgba(0,0,0,0.14), 0px -1px 5px 0px rgba(0,0,0,0.12)"}}>
         <div class="main-footer widgets-dark typo-light">
             <div class="container">
                 <div class="row">
