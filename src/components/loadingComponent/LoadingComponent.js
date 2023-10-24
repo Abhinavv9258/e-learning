db3c3b2194fb16c3b13513100ddefbb942e1d88a
Abhinav Verma <79451162+Abhinavv9258@users.noreply.github.com>
2023-10-24 20:06:07 +0530

    Updated Loading Card Component

--- src/components/loadingComponent/LoadingComponent.js
+++ src/components/loadingComponent/LoadingComponent.js
@@ -1,32 +1,36 @@
 import React from 'react';
-import './loading.css';
-import './loadingSkeleton.css';
-import {Box, Typography} from '@mui/material';
+import './LoadingComponent.css';
+import { Card, CardHeader, Box, Typography, CardContent } from '@mui/material';
+
 
 const LoadingComponent = () => {
     return (
-        <Box
-
-            display='flex'
-            flexDirection='column'
-            justifyContent='center'
-            boxShadow={5}
-            p={4}
-            m={2}
-            _hover={{ boxShadow: 10, cursor: 'pointer' }}
-        >
-            
-            <span className='loaderSkeleton'></span>
-
-            <Box>
-                <Typography variant='h4' display='flex'
-                    flexDirection='row'
-                    alignItems='center'
-                    >
-                    L<Box className='loader'></Box>ading
-                </Typography>
-            </Box>
-        </Box>
+        <>
+            <Card
+                sx={{ m: 2, p: 2, boxShadow: 3, height: 380 }}
+            >
+                <CardHeader
+                    title={
+                        <Box
+                            display='flex'
+                            flexDirection='column'
+                            justifyContent='center'
+                        >
+                            <span className='loaderSkeleton'></span>
+                        </Box>
+                    }
+                />
+                <CardContent>
+                    <Box>
+                        <Typography variant='h5' display='flex'
+                            flexDirection='row'
+                            alignItems='center'
+                        >
+                            L<Box className='loader'></Box>ading
+                        </Typography>
+                    </Box>
+                </CardContent>
+            </Card></>
     );
 };
 
