556c32d3f866b155a38dd59a5af79a1889e3fc5f
Abhinav Verma <79451162+Abhinavv9258@users.noreply.github.com>
2023-11-12 23:20:19 +0530

    Create LoginPopUp.js

---
+++ src/components/modals/LoginPopUp.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Button,
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const LoginPopUp = ({ open, onClose, setLoginPopUp }) => {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        setLoginPopUp(false);
+        navigate('/user-login-page');
+    }
+
+    return (
+        <>
+            <Dialog open={open} onClose={onClose}>
+                <DialogTitle>Confirmation</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To access this feature, please log in.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleLogin} color="primary">
+                        Login
+                    </Button>
+                    <Button onClick={onClose} color="error">
+                        Back
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+};
+
+export default LoginPopUp;
